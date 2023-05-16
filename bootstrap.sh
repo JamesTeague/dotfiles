a40bcd82115aa468c0b7988f42ea7afceee3d65c
JamesTeague <james@teague.dev>
2023-05-15 22:35:55 -0500

    bootstrap packer for neovim

--- bootstrap.sh
+++ bootstrap.sh
@@ -117,4 +117,8 @@ mv ~/.ideavimrc ~/.ideavimrc.pre_bootstrap
 echo Linking .ideavimrc for IntelliJ...
 ln -sf $(pwd)/ideavimrc $(echo $HOME)/.ideavimrc
 
+echo Installing packer for neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 echo Done. Run "source ~/.zshrc" or open a new terminal.
