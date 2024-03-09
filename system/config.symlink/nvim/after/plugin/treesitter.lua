23fabd457e50040cbf7b53e1dde92194d9ae3a12
James Teague II <james@teague.dev>
2024-03-08 22:28:27 -0600

    add languages to treesitter

--- system/config.symlink/nvim/after/plugin/treesitter.lua
+++ system/config.symlink/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,19 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "go", "rust", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = {
+    "javascript",
+    "typescript",
+    "go",
+    "rust",
+    "c",
+    "lua",
+    "query",
+    "sql",
+    "helm",
+    "terraform",
+    "markdown",
+    "markdown_inline",
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
