5bd0b4233bf1886943b7323711675d34115cb91a
James Teague II <james@teague.dev>
2024-03-10 16:29:12 -0500

    seperate plugins to their own files
    add cloak, highlight undo, leap, project, and trouble too

--- system/config.symlink/nvim/after/plugin/floaterm.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "<leader>ft", ":FloatermNew --name=floater --height=0.8 --width=0.7 --autoclose=2 zsh <CR> ")
-vim.keymap.set('n', "t", ":FloatermToggle floater<CR>")
-vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>")
-vim.keymap.set('t', "<C-s>", "<C-\\><C-n>")
-vim.keymap.set("t", "<C-n>", ":FloatermNext")
-vim.keymap.set("t", "<C-p>", ":FloatermPrev")
-vim.keymap.set("t", "<C-k>", ":FloatermKill")
