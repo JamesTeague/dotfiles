49474b0f0938b49f271798687fca26e9d84d0f7c
James Teague II <james@teague.dev>
2024-03-14 23:13:34 -0500

    add debug icons with colors

--- system/config.symlink/nvim/lua/autocmds.lua
+++ system/config.symlink/nvim/lua/autocmds.lua
@@ -12,6 +12,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Prevent ColorScheme clears self-defined DAP icon colors
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  desc = "Prevent colorscheme clears self-defined DAP icon colors.",
+  callback = function()
+    vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939' })
+    vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef' })
+    vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379' })
+  end
+})
+
+-- reload current color scheme to pick up colors override if it was set up in a lazy plugin definition fashion
+vim.cmd.colorscheme(vim.g.colors_name)
+
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
