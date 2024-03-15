3b45a5f10f816ce235486b603fb5e9f9c1c4927c
James Teague II <james@teague.dev>
2024-03-14 23:11:11 -0500

    change leap to use leader as to not conflict with vim

--- system/config.symlink/nvim/lua/plugins/leap.lua
+++ system/config.symlink/nvim/lua/plugins/leap.lua
@@ -2,9 +2,9 @@ return {
   {
     'ggandor/leap.nvim',
     keys = {
-      { 'f', '<Plug>(leap-forward)', mode = {'n', 'x', 'o'}, desc = 'Leap Forward' },
-      { 'F', '<Plug>(leap-backward)', mode = {'n', 'x', 'o'}, desc = 'Leap Backward' },
-      { 'fs', '<Plug>(leap-from-window)', mode = {'n', 'x', 'o'}, desc = 'Leap From Window' },
+      { '<leader>f', '<Plug>(leap-forward)', mode = {'n', 'x', 'o'}, desc = 'Leap Forward' },
+      { '<leader>F', '<Plug>(leap-backward)', mode = {'n', 'x', 'o'}, desc = 'Leap Backward' },
+      { '<leader>fs', '<Plug>(leap-from-window)', mode = {'n', 'x', 'o'}, desc = 'Leap From Window' },
     },
   },
 }
