cbd612b8ec3e99d3b835f7a0088aa8998253419c
James Teague II <james@teague.dev>
2024-03-15 22:34:12 -0500

    add keymaps for diffing in git

--- system/config.symlink/nvim/lua/plugins/git.lua
+++ system/config.symlink/nvim/lua/plugins/git.lua
@@ -8,6 +8,9 @@ return {
     },
     keys = {
       { '<leader>gs', '<cmd>Neogit<cr>', desc = '[G]it [S]tatus' },
+      { '<leader>grp', '<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<cr>', desc = '[G]it [R]eview [P]ull' },
+      { '<leader>grpc', '<cmd>DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges<cr>', desc = '[G]it [R]eview [P]ull [C]ommits' },
+      { '<leader>grs', '<cmd>DiffviewFileHistory -g --range=stash<cr>', desc = '[G]it [R]eview [S]tash' },
     },
     config = true
   },
