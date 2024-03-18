bc90c7fcc992a835366677c40eea50538431fda6
James Teague II <james@teague.dev>
2024-03-17 23:45:32 -0500

    add neotest for testing

---
+++ system/config.symlink/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/neotest-go',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      -- Your other test adapters here
+    },
+    config = function()
+      require('neotest').setup({
+        -- your neotest config here
+        adapters = {
+          require('neotest-go'),
+        },
+      })
+      vim.keymap.set('n', '<leader>tm', require('neotest').run.run, { desc = '[T]est [M]ethod' })
+      vim.keymap.set('n', '<leader>tf', function() require('neotest').run.run(vim.fn.expand('%')) end,
+        { desc = '[T]est [F]ile' })
+      vim.keymap.set('n', '<leader>td', function() require('neotest').run.run(vim.fn.input('Path to Directory:')) end,
+        { desc = '[T]est [D]irectory' })
+      vim.keymap.set('n', '<leader>ts', function() require('neotest').run.run(vim.fn.getcwd()) end,
+        { desc = '[T]est [S]uite' })
+    end,
+  },
+}
