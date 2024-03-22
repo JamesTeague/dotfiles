566b8b7fea9550c05c74d1b93287df0c38b32ec2
JamesTeague <james@teague.dev>
2024-03-22 15:30:06 -0500

    change keymaps for leap to m

--- system/config.symlink/nvim/lua/plugins/leap.lua
+++ system/config.symlink/nvim/lua/plugins/leap.lua
@@ -2,9 +2,9 @@ return {
   {
     'ggandor/leap.nvim',
     keys = {
-      { '<leader>f', '<Plug>(leap-forward)', mode = {'n', 'x', 'o'}, desc = 'Leap Forward' },
-      { '<leader>F', '<Plug>(leap-backward)', mode = {'n', 'x', 'o'}, desc = 'Leap Backward' },
-      { '<leader>fs', '<Plug>(leap-from-window)', mode = {'n', 'x', 'o'}, desc = 'Leap From Window' },
+      { 'm',  '<Plug>(leap-forward)',     mode = { 'n', 'x', 'o' }, desc = 'Leap Forward' },
+      { 'M',  '<Plug>(leap-backward)',    mode = { 'n', 'x', 'o' }, desc = 'Leap Backward' },
+      { 'ms', '<Plug>(leap-from-window)', mode = { 'n', 'x', 'o' }, desc = 'Leap From Window' },
     },
   },
 }
