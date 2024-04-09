a5f8653c0e4c37c0c9f197b9fe5571c0882d36e3
James Teague <james@teague.dev>
2024-04-09 14:51:26 -0500

    fix(ohmyzsh): move dotfile back if overwritten

--- home/dot_topics/oh-my-zsh/executable_install.sh
+++ home/dot_topics/oh-my-zsh/executable_install.sh
@@ -3,6 +3,10 @@
 if [ -z $ZSH ] || [ ! -d $ZSH ]; then
   echo "› Configuring oh-my-zsh"
   sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  if [ -f ~/.zshrc.pre-oh-my-zsh ]; then
+    mv -f .zshrc.pre-oh-my-zsh .zshrc
+  fi
 else
   echo "Oh-my-zsh already installed"
 fi
