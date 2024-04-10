f4a21fd7d23e3a1a178f22a449a83bee1c2002c9
James Teague II <JamesTeague@users.noreply.github.com>
2024-04-10 12:13:06 -0500

    Update generate-gpg-key.sh

--- home/scripts/generate-gpg-key.sh
+++ home/scripts/generate-gpg-key.sh
@@ -16,7 +16,8 @@ if [[ -n "$existing_keys" ]]; then
   gpg_key_id=$(echo "$existing_keys" | grep -E "^sec" | awk '{print $2}' | cut -d '/' -f 2)
   # info "You already have an existing GPG key. Your GPG key ID is: $gpg_key_id"
   # git config --global user.signingkey $gpg_key_id 
-  echo $gpg_key_id
+  # echo $gpg_key_id
+  
 else
   # Generate a new GPG key
   $gpg --full-generate-key
@@ -25,8 +26,22 @@ else
   gpg_key_id=$($gpg --list-secret-keys --keyid-format LONG | grep -E "^sec" | awk '{print $2}' | cut -d '/' -f 2)
   # echo "GPG key generated successfully. Your GPG key ID is: $gpg_key_id"
   # git config --global user.signingkey $gpg_key_id     
-  echo $gpg_key_id
+  # echo $gpg_key_id
 fi
 
 # Print the GPG public key
 # gpg --armor --export $gpg_key_id
+cat <<'EOF'
+[user]
+  name = {{ .name }}
+  email = {{ .email }}
+  signingkey = ${gpg_key_id}
+[credential]
+  {{ if eq .chezmoi.os "darwin" -}}
+  helper = osxkeychain
+  {{ else if eq .chezmoi.os "linux" -}}
+  helper = cache
+  {{ end -}}
+[commit]
+  gpgsign = true
+EOF
