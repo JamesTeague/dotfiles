ccf605a5eeda11bb5374fd6757c3cb5a3e2aabb0
James Teague <james@teague.dev>
2024-04-18 16:51:26 -0500

    feat(lsp): add rust-analyzer

--- home/private_dot_config/nvim/lua/plugins/go.lua
+++ home/private_dot_config/nvim/lua/plugins/go.lua
@@ -11,31 +11,6 @@ return {
       { "<leader>gp", ":GoPkgOutline<CR>", { desc = "View Go Package Outline" } },
       { "<leader>gm", ":GoMockGen -s<CR>", { desc = "Generate Go Mock for Interface by source" } },
     },
-    -- config = function()
-    --   vim.env.TEST_ENV = "local"
-    --
-    --   vim.keymap.set(
-    --     "n",
-    --     "<leader>gt",
-    --     [[:let $TEST_ENV = 'local' | GoTestFunc<CR>]],
-    --     { desc = "Run Nearest [G]o [T]est" }
-    --   )
-    --   vim.keymap.set(
-    --     "n",
-    --     "<leader>gts",
-    --     [[:let $TEST_ENV = 'local' | GoTestFunc -s <CR>]],
-    --     { desc = "Select Go Tests to run" }
-    --   )
-    --   vim.keymap.set(
-    --     "n",
-    --     "<leader>gtf",
-    --     [[:let $TEST_ENV = 'local' | GoTestFile <CR>]],
-    --     { desc = "Run [G]o [T]ests in [F]ile" }
-    --   )
-    --
-    --   vim.keymap.set("n", "<leader>gp", ":GoPkgOutline<CR>", { desc = "View Go Package Outline" })
-    --   vim.keymap.set("n", "<leader>gm", ":GoMockGen -s<CR>", { desc = "Generate Go Mock for Interface by source" })
-    -- end,
   },
   "ray-x/guihua.lua", -- recommended if need floating window support
   {
