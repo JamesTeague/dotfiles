a04c3ec5021a219791b2307ee49a34ae89b75ccc
James Teague <james@teague.dev>
2024-05-19 09:57:33 -0500

    dx(dap): add keymaps for eval and run to cursor

--- home/private_dot_config/nvim/lua/configs/dap.lua
+++ home/private_dot_config/nvim/lua/configs/dap.lua
@@ -51,17 +51,26 @@ vim.keymap.set("n", "<F9>", dap.continue, { desc = "Debug: Start/Continue" })
 vim.keymap.set("n", "<F7>", dap.step_into, { desc = "Debug: Step Into" })
 vim.keymap.set("n", "<F8>", dap.step_over, { desc = "Debug: Step Over" })
 vim.keymap.set("n", "<F6>", dap.step_out, { desc = "Debug: Step Out" })
+vim.keymap.set("n", "<F5>", dapui.toggle, { desc = "Debug: See last session result." })
+vim.keymap.set("n", "<F4>", dap.run_to_cursor, { desc = "Debug: Run To Cursor" })
 vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+
 vim.keymap.set("n", "<leader>B", function()
   dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 end, { desc = "Debug: Set Breakpoint" })
+
 vim.keymap.set("n", "<leader>bc", function()
   dap.clear_breakpoints()
   require("notify")("Breakpoints cleared", "warn")
 end, { desc = "Debug: Clear Breakpoints" })
+
 vim.keymap.set("n", "<leader>bl", function()
   dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 end, { desc = "Debug: Set Logpoint" })
+
+vim.keymap.set("n", "<leader>?", function()
+  require("dapui").eval(nil, { enter = true })
+end)
 
 vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint" })
 vim.fn.sign_define("DapBreakpointCondition", { text = "◉", texthl = "DapBreakpoint" })
@@ -85,8 +94,6 @@ dapui.setup({
     },
   },
 })
-
-vim.keymap.set("n", "<F5>", dapui.toggle, { desc = "Debug: See last session result." })
 
 dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 dap.listeners.before.event_terminated["dapui_config"] = dapui.close
