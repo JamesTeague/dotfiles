d014e0f1feaf14f9a9f61f222eff6769d403680e
James Teague II <JamesTeague@users.noreply.github.com>
2024-05-23 13:41:46 -0500

    Update remove-bloat.ps1

--- windows/remove-bloat.ps1
+++ windows/remove-bloat.ps1
@@ -4,7 +4,6 @@ Get-AppxPackage -AllUsers Microsoft.BingWeather | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.GetHelp | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.Getstarted | Remove-AppPackage
 Get-AppxPackage -AllUsers Microsoft.Microsoft3DViewer | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.MixedReality.Portal | Remove-AppxPackage
@@ -14,14 +13,12 @@ Get-AppxPackage -AllUsers Microsoft.ScreenSketch | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.SkypeApp | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.StorePurchaseApp | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsAlarms | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsCalculator | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsCamera | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsCommunicationsApps | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsFeedbackHub | Remove-AppPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsMaps | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.Windows.Photos | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsSoundRecorder | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsStore | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.XboxApp | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.XboxGameOverlay | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
