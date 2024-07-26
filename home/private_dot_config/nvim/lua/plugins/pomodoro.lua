ca40b3202551b0d85792b8a986fb7f0b2b7e65e0
James Teague <james@teague.dev>
2024-07-26 10:29:34 -0500

    feat(pomodoro): add pomodoro plugin

---
+++ home/private_dot_config/nvim/lua/plugins/pomodoro.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "wthollingsworth/pomodoro.nvim",
+    depenencies = "MunifTanjim/nui.nvim",
+    opts = {
+      time_work = 25,
+      time_break_short = 5,
+      time_break_long = 20,
+      timers_to_long_break = 4,
+    },
+    keys = {
+      {
+        "<leader>pt",
+        "<CMD>:PomodoroStart<CR>",
+        desc = "[P]omodoro Start Timer",
+      },
+      {
+        "<leader>pp",
+        "<CMD>:PomodoroStatus<CR>",
+        desc = "[P]omodoro Status",
+      },
+      {
+        "<leader>ps",
+        "<CMD>:PomodoroStop<CR>",
+        desc = "[P]omodoro Stop Timer",
+      },
+    },
+  },
+}
