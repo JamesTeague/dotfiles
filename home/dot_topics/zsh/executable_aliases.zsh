b113729272c28b567d4ffc35cbc6a16abded84c6
James Teague <james@teague.dev>
2024-10-14 17:13:41 -0500

    shortcuts for git commands

--- home/dot_topics/zsh/executable_aliases.zsh
+++ home/dot_topics/zsh/executable_aliases.zsh
@@ -1,5 +1,7 @@
 alias prune-branches="git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d"
-alias vim="nvim"
+alias git-list-untracked='git fetch --prune && git branch -r | awk "{print \$1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print \$1}"'
+alias git-remove-untracked='git fetch --prune && git branch -r | awk "{print \$1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print \$1}" | xargs git branch -d'alias vim="nvim"
+alias wt='git worktree'
 alias b="buku --suggest"
 alias reload='. ~/.zshrc'
 alias lg='lazygit'
